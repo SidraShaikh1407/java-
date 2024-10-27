1932df8b3510b8e6933b38f794c08dd7b9681325
SidraShaikh1407 <sidrashaikh8455@gmail.com>
2024-10-28 01:41:14 +0530

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,18 @@
+package skll_oop_B3_odd;
+
+public class Add {
+ int num1;
+ int num2;
+ Add(int n, int m) {
+	 num1=n;
+	 num2=m;
+ }
+  public void getAddition()  {
+	  int result;
+	  result=num1+num2;
+	  System.out.println("The addition of two number is "+ result);  
+	  }
+ 
+	}
+
+
---
+++ AddDemo.java
@@ -0,0 +1,11 @@
+package skll_oop_B3_odd;
+
+import  skll_oop_B3_odd.Add;
+public class AddDemo {
+
+	public static void main(String[] args) {
+		Add obj=new Add(20,14);
+        obj.getAddition();
+	}
+
+}
